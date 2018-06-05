df1254efdd58a56a6f862530020d3af305fa7e4c
guonanci <nanci_guo@xinyan.com>
2018-06-05 23:29:08 +0800

    ci.sh

--- ci.sh
+++ ci.sh
@@ -1,5 +1,7 @@
 # daily push
 git add .
 git commit -m $1
+# git pull is too slowly, maybe its because that
+# Warning: Permanently added the RSA host key for IP address '52.74.223.119' to the list of known hosts.
 git pull
 git push
