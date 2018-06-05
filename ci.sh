dda8bd6c19121c85d58c93db67d2cb8915923f72
guonanci <nanci_guo@xinyan.com>
2018-06-05 23:49:30 +0800

    two-badges

--- ci.sh
+++ ci.sh
@@ -3,5 +3,7 @@ git add .
 git commit -m $1
 # git pull is too slowly, maybe its because that
 # Warning: Permanently added the RSA host key for IP address '52.74.223.119' to the list of known hosts.
+# but ip is always changing
+#mayge i should always choose shadowsocks auto-proxy mode!!
 git pull
 git push
