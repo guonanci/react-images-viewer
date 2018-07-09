ef447e57ef0e4c1e1ce78f344924bfc4f24f672f
guonanci <yiminanci@gmail.com>
2018-07-09 13:30:59 +0800

    index.html

--- examples/src/components/Standard.js
+++ examples/src/components/Standard.js
@@ -16,7 +16,7 @@ var Standard = React.createClass({
     evnet.preventDefault()
     this.setState({
       lightboxIsOpen: true,
-      lightboxInialImg: index
+      lightboxInitialImg: index
     })
   },
   closeLightbox() {
@@ -29,5 +29,51 @@ var Standard = React.createClass({
     if (!this.props.imgs) {
       return
     }
+  },
+  render() {
+    return (
+      <div>
+        <h2>Standard</h2>
+        {this.renderGallery()}
+        <Lightbox
+          imgs={this.props.imgs}
+          initialImg={this.state.lightboxInitialImg}
+          isOpen={this.state.lightboxIsOpen}
+          onCancel={this.closeLightbox}
+        />
+      </div>
+    )
   }
 })
+
+const THUMANAIL_SIZE = 58
+
+const styles = {
+  gallery: {
+    marginLeft: -5,
+    marginRight: -5,
+    overflow: 'hidden'
+  },
+  thumbnail: {
+    bgSize: 'cover',
+    borderRadius: 3,
+    float: 'left',
+    height: THUMBNAIL_SIZE,
+    margin: 5,
+    overflow: 'hidden',
+    width: THUMANAIL_SIZE
+  },
+  thumbnailImg: {
+    display: 'block',
+    height: THUMBNAIL_SIZE,
+    left: '50%',
+    position: 'relative',
+
+    WebkitTransform: 'translateX(-50%)',
+    MozTransform: 'translateX(-50%)',
+    msTransform: 'translateX(-50%)',
+    transform: 'translateX(-50%)'
+  }
+}
+
+module.exports = Standard
