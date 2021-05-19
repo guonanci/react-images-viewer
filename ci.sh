d8521a5e047389291252eb04b7e502def9293010
guonanci <yiminanci@gmail.com>
2021-05-19 21:33:13 +0800

    fix(version): upd

--- ci.sh
+++ ci.sh
@@ -1,10 +1,7 @@
-# daily push
-git pull
+#!/bin/bash -e
+# daily push, but add all unStaged changes.
+commit_msg="$1"
 git add .
-git commit -m $1
-# git pull is too slowly, maybe its because that
-# Warning: Permanently added the RSA host key for IP address '52.74.223.119' to the list of known hosts.
-# but ip is always changing
-# mayge i should always choose shadowsocks auto-proxy mode!!
-# I finally know CMCC-WIFI often changes IP, but github fucks it.
+git commit -m "$commit_msg"
+git pull
 git push
