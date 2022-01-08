88d240e31b86d8cd2916b23494a5d9a09dd4a3fe
nanci <yiminanci@gmail.com>
2022-01-08 11:07:24 +0800

    fix(icons): icons/arrow_left,arrow_right imports

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,28 +1,23 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import arrowLeft from '../icons/ArrowLeft'
-import arrowRight from '../icons/ArrowRight'
-import close from '../icons/Close'
+import PropTypes from "prop-types";
+import React from "react";
+import arrowLeft from "../icons/arrow_left";
+import arrowRight from "../icons/arrow_right";
+import close from "../icons/Close";
 
-const icons = { arrowLeft, arrowRight, close }
+const icons = { arrowLeft, arrowRight, close };
 
 const Icon = ({ fill, type, ...props }) => {
-  const icon = icons[type]
+  const icon = icons[type];
 
-  return (
-    <span
-      dangerouslySetInnerHTML={{ __html: icon(fill) }}
-      {...props}
-    />
-  )
-}
+  return <span dangerouslySetInnerHTML={{ __html: icon(fill) }} {...props} />;
+};
 
 Icon.propTypes = {
   fill: PropTypes.string,
   type: PropTypes.oneOf(Object.keys(icons)),
-}
+};
 Icon.defaultProps = {
-  fill: '#fff',
-}
+  fill: "#fff",
+};
 
-export default Icon
+export default Icon;
